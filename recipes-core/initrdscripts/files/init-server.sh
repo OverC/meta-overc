6fb7e92be32ad7c8b2f2a702e866714290273363
Paul Gortmaker <paul.gortmaker@windriver.com>
2014-07-09 14:10:49 -0400

    builder: add support for an initramfs
    It will be built as part of the "yocto-builder" target.  To deploy
    it, you'll need to take it from tmp/deploy and add it to /boot of
    your system rootfs, and update grub with an initrd line.
    
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

---
+++ recipes-core/initrdscripts/files/init-server.sh
@@ -0,0 +1,105 @@
+#!/bin/sh
+
+PATH=/sbin:/bin:/usr/sbin:/usr/bin
+
+ROOT_MOUNT="/rootfs"
+MOUNT="/bin/mount"
+UMOUNT="/bin/umount"
+
+# Copied from initramfs-framework. The core of this script probably should be
+# turned into initramfs-framework modules to reduce duplication.
+udev_daemon() {
+	OPTIONS="/sbin/udev/udevd /sbin/udevd /lib/udev/udevd /lib/systemd/systemd-udevd"
+
+	for o in $OPTIONS; do
+		if [ -x "$o" ]; then
+			echo $o
+			return 0
+		fi
+	done
+
+	return 1
+}
+
+_UDEV_DAEMON=`udev_daemon`
+
+early_setup() {
+    mkdir -p /proc
+    mkdir -p /sys
+    mount -t proc proc /proc
+    mount -t sysfs sysfs /sys
+    mount -t devtmpfs none /dev
+
+    # support modular kernel
+#    modprobe isofs
+#    modprobe raid0
+
+    mkdir -p /run
+    mkdir -p /var/run
+
+    $_UDEV_DAEMON --daemon
+    udevadm trigger --action=add
+
+    echo Running RAID setup
+
+    if [ -x /sbin/mdadm ]; then
+	/sbin/mdadm -v --assemble --scan --auto=md
+    fi
+}
+
+read_args() {
+    [ -z "$CMDLINE" ] && CMDLINE=`cat /proc/cmdline`
+    for arg in $CMDLINE; do
+        optarg=`expr "x$arg" : 'x[^=]*=\(.*\)'`
+        case $arg in
+            root=*)
+                ROOT_DEVICE=$optarg ;;
+            console=*)
+                if [ -z "${console_params}" ]; then
+                    console_params=$arg
+                else
+                    console_params="$console_params $arg"
+                fi ;;
+        esac
+    done
+}
+
+fatal() {
+    echo $1 >$CONSOLE
+    echo >$CONSOLE
+    exec sh
+}
+
+
+
+#######################################
+
+early_setup
+
+[ -z "$CONSOLE" ] && CONSOLE="/dev/console"
+
+read_args
+
+udevadm settle --timeout=3 --quiet
+killall "${_UDEV_DAEMON##*/}" 2>/dev/null
+
+mkdir -p $ROOT_MOUNT/
+
+if ! mount -o rw,noatime $ROOT_DEVICE $ROOT_MOUNT ; then
+    fatal "Could not mount rootfs device \"$ROOT_DEVICE\"."
+fi
+
+# Move the mount points of some filesystems over to
+# the corresponding directories under the real root filesystem.
+echo Moving dirs to ${ROOT_MOUNT}
+for dir in `cat /proc/mounts | grep -v rootfs | awk '{print $2}'` ; do
+    echo Moving \"$dir\". to ${ROOT_MOUNT}
+    mkdir -p  ${ROOT_MOUNT}/${dir##*/}
+    mount -n --move $dir ${ROOT_MOUNT}/${dir##*/}
+done
+
+cd $ROOT_MOUNT
+
+# busybox switch_root supports -c option
+exec switch_root -c /dev/console $ROOT_MOUNT /sbin/init $CMDLINE ||
+    fatal "Couldn't switch_root, dropping to shell"
---
+++ recipes-core/initrdscripts/initramfs-yocto-builder.bb
@@ -0,0 +1,22 @@
+SUMMARY = "Basic init for initramfs to mount and pivot root"
+LICENSE = "MIT"
+LIC_FILES_CHKSUM = "file://${COREBASE}/meta/COPYING.MIT;md5=3da9cfbcb788c80a0384361b4de20420"
+SRC_URI = "file://init-server.sh"
+
+PR = "r9"
+
+RDEPENDS_${PN} = "grub parted e2fsprogs-mke2fs"
+
+do_install() {
+        install -m 0755 ${WORKDIR}/init-server.sh ${D}/init
+}
+
+# While this package maybe an allarch due to it being a 
+# simple script, reality is that it is Host specific based
+# on the COMPATIBLE_HOST below, which needs to take precedence
+#inherit allarch
+INHIBIT_DEFAULT_DEPS = "1"
+
+FILES_${PN} = " /init "
+
+COMPATIBLE_HOST = "(i.86|x86_64).*-linux"
