19bc1e8341856775e5d2e0146ec1157481a43e6e
Feng Mu <Feng.Mu@windriver.com>
2016-08-02 14:14:24 +0800

    scripts: reindent using tab
    Currently the indent is mixed with tab and spaces, which is very
    confusing when reading the code. So reindent using pure tabs.
    
    Signed-off-by: Feng Mu <Feng.Mu@windriver.com>

--- meta-cube/recipes-core/initrdscripts/files/init-server.sh
+++ meta-cube/recipes-core/initrdscripts/files/init-server.sh
@@ -25,46 +25,46 @@ udev_daemon() {
 _UDEV_DAEMON=`udev_daemon`
 
 early_setup() {
-    mkdir -p /proc
-    mkdir -p /sys
-    mount -t proc proc /proc
-    mount -t sysfs sysfs /sys
-    mount -t devtmpfs none /dev
+	mkdir -p /proc
+	mkdir -p /sys
+	mount -t proc proc /proc
+	mount -t sysfs sysfs /sys
+	mount -t devtmpfs none /dev
 
-    # support modular kernel
-#    modprobe isofs
-#    modprobe raid0
+	# support modular kernel
+#	modprobe isofs
+#	modprobe raid0
 
-    mkdir -p /run
-    mkdir -p /var/run
+	mkdir -p /run
+	mkdir -p /var/run
 
-    $_UDEV_DAEMON --daemon
-    udevadm trigger --action=add
+	$_UDEV_DAEMON --daemon
+	udevadm trigger --action=add
 
-    if [ -x /sbin/mdadm ]; then
-	/sbin/mdadm -v --assemble --scan --auto=md
-    fi
+	if [ -x /sbin/mdadm ]; then
+		/sbin/mdadm -v --assemble --scan --auto=md
+	fi
 }
 
 read_args() {
-    [ -z "$CMDLINE" ] && CMDLINE=`cat /proc/cmdline`
-    for arg in $CMDLINE; do
-        optarg=`expr "x$arg" : 'x[^=]*=\(.*\)'`
-        case $arg in
-            root=*)
-                ROOT_DEVICE=$optarg ;;
-            rootdelay=*)
-                ROOT_DELAY=$optarg ;;
-            init=*)
-                INIT=$optarg ;;
-        esac
-    done
+	[ -z "$CMDLINE" ] && CMDLINE=`cat /proc/cmdline`
+	for arg in $CMDLINE; do
+		optarg=`expr "x$arg" : 'x[^=]*=\(.*\)'`
+		case $arg in
+			root=*)
+				ROOT_DEVICE=$optarg ;;
+			rootdelay=*)
+				ROOT_DELAY=$optarg ;;
+			init=*)
+				INIT=$optarg ;;
+		esac
+	done
 }
 
 fatal() {
-    echo $1 >$CONSOLE
-    echo >$CONSOLE
-    exec sh
+	echo $1 >$CONSOLE
+	echo >$CONSOLE
+	exec sh
 }
 
 
@@ -87,14 +87,14 @@ mkdir -p $ROOT_MOUNT/
 sleep ${ROOT_DELAY}
 
 if ! mount -o rw,noatime $ROOT_DEVICE $ROOT_MOUNT ; then
-    fatal "Could not mount rootfs device \"$ROOT_DEVICE\"."
+	fatal "Could not mount rootfs device \"$ROOT_DEVICE\"."
 fi
 
 # Move the mount points of some filesystems over to
 # the corresponding directories under the real root filesystem.
 for dir in `cat /proc/mounts | grep -v rootfs | awk '{print $2}'` ; do
-    mkdir -p  ${ROOT_MOUNT}/${dir##*/}
-    mount -nv --move $dir ${ROOT_MOUNT}/${dir##*/}
+	mkdir -p  ${ROOT_MOUNT}/${dir##*/}
+	mount -nv --move $dir ${ROOT_MOUNT}/${dir##*/}
 done
 
 cd $ROOT_MOUNT
@@ -102,8 +102,8 @@ cd $ROOT_MOUNT
 # If we pass args to bash, it will assume they are text files
 # to source and run.
 if [ "$INIT" == "/bin/bash" ] || [ "$INIT" == "/bin/sh" ]; then
-    CMDLINE=""
+	CMDLINE=""
 fi
 
 exec switch_root $ROOT_MOUNT $INIT $CMDLINE ||
-    fatal "Couldn't switch_root, dropping to shell"
+	fatal "Couldn't switch_root, dropping to shell"
