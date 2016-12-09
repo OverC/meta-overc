1cce564e7cb18dc9ee7eae94a89a2e2de609794d
Jason Wessel <jason.wessel@windriver.com>
2016-12-08 16:32:31 -0800

    overc-system-agent: Add support for raspberry pi
    The correct kernel file needs to be checked to avoid this error:
    
    Traceback (most recent call last):
      File "/opt/overc-system-agent/overc", line 51, in <module>
        overc=Overc.Overc()
      File "/opt/overc-system-agent/Overc/overc.py", line 31, in __init__
        self.agency = Host_update()
      File "/opt/overc-system-agent/Overc/backends/btrfs.py", line 26, in __init__
        self.kernel_md5 = self._compute_checksum(self.kernel)
    AttributeError: 'Btrfs' object has no attribute 'kernel'
    
    Signed-off-by: Jason Wessel <jason.wessel@windriver.com>
    Signed-off-by: Bruce Ashfield <bruce.ashfield@windriver.com>

--- meta-cube/recipes-support/overc-system-agent/files/overc-system-agent-1.2/Overc/backends/btrfs.py
+++ meta-cube/recipes-support/overc-system-agent/files/overc-system-agent-1.2/Overc/backends/btrfs.py
@@ -22,6 +22,8 @@ class Btrfs(Utils):
             self.kernel = "/boot/uImage"
         elif os.path.exists('/boot/zImage'):
             self.kernel = "/boot/zImage"
+        elif os.path.exists('/boot/kernel7.img'):
+            self.kernel = "/boot/kernel7.img"
 
         self.kernel_md5 = self._compute_checksum(self.kernel)
 
