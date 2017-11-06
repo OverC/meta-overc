5f4a325b1ef7ae69c967b3ffae49d64392a0ee13
Fupan Li <fupan.li@windriver.com>
2017-11-06 05:23:46 +0000

    overc-conftool: create the system-id file for both of essential and cube containers
    DNF need the system-id to access the rpm repo server, thus
    create this file for both essential and cube containers
    by ansible at the first boottime.
    
    Signed-off-by: Fupan Li <fupan.li@windriver.com>
    Signed-off-by: Bruce Ashfield <bruce.ashfield@windriver.com>

--- meta-cube/recipes-support/overc-conftools/source/system/systemid-set.sh
+++ meta-cube/recipes-support/overc-conftools/source/system/systemid-set.sh
@@ -1,10 +1,16 @@
 #!/bin/sh
 
 linkdevs="wl* en* eth*"
+containers="dom0 cube-gw cube-server cube-desktop"
 
 #Use one physical mac address as the system id
 for dev in `(cd /sys/class/net && ls -d $linkdevs 2>/dev/null)`; do
     cat  /sys/class/net/$dev/address >/etc/system-id
+    for cn in $containers; do
+        if [ -d /opt/container/$cn/rootfs/etc ]; then
+            cat /sys/class/net/$dev/address >/opt/container/$cn/rootfs/etc/system-id
+        fi
+    done
     break
 done
 
