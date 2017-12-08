OverC/meta-overc

commit eef4b5948643dcd464d5336a1fc2482ffd649505

overc-system-agent: overc: drop container_cleanup
It's not needed any more, it's already being handled by dom0 template
and overc-ctl.

Signed-off-by: Ming Liu <liu.ming50@gmail.com>

meta-cube/recipes-support/overc-system-agent/files/overc-system-agent-1.2/Overc/backends/btrfs.py
@@ -146,12 +146,6 @@ class Btrfs(Utils):
         self._mount_rootvolume(True)
         self._cleanup_subvol(SYSROOT, [FACTORY_SNAPSHOT, 'rootfs_bakup'], True)
 
-    def clean_container(self):
-        self._mount_container_root()
-        current_subvol = self._get_btrfs_value(CONTAINER_MOUNT, 'Name')
-        self._cleanup_subvol('%s/.tmp' % CONTAINER_MOUNT, [current_subvol, FACTORY_SNAPSHOT])
-        os.system('umount %s/.tmp' % CONTAINER_MOUNT)
-        
     def factory_reset(self):
         return self._factory_reset_essential() and self._factory_reset_container() 
 

meta-cube/recipes-support/overc-system-agent/files/overc-system-agent-1.2/factory_clean.py
@@ -7,4 +7,3 @@ import Overc
 if __name__ == '__main__':
     overc=Overc.Overc()
     overc.agency.clean_essential()
-    overc.agency.clean_container() 

commit cd40d67710192b7d997558458b5d03066c1b41bf

overc-system-agent: btrfs.py: add clean_container back
A regression was introduce by commit eef4b594:
[ overc-system-agent: overc: drop container_cleanup ]

it should not be removed, or else there will be snapshot leftovers in
container directory.

Signed-off-by: Ming Liu <liu.ming50@gmail.com>

meta-cube/recipes-support/overc-system-agent/files/overc-system-agent-1.2/Overc/backends/btrfs.py
@@ -146,6 +146,12 @@ class Btrfs(Utils):
         self._mount_rootvolume(True)
         self._cleanup_subvol(SYSROOT, [FACTORY_SNAPSHOT, 'rootfs_bakup'], True)
 
+    def clean_container(self):
+        self._mount_container_root(True)
+        current_subvol = self._get_btrfs_value(CONTAINER_MOUNT, 'Name', True)
+        self._cleanup_subvol('%s/.tmp' % CONTAINER_MOUNT, [current_subvol, FACTORY_SNAPSHOT], True)
+        os.system('cube-cmd umount %s/.tmp' % CONTAINER_MOUNT)
+
     def factory_reset(self):
         return self._factory_reset_essential() and self._factory_reset_container() 
 

meta-cube/recipes-support/overc-system-agent/files/overc-system-agent-1.2/Overc/overc.py
@@ -294,11 +294,14 @@ class Overc(object):
         sys.exit(self.retval)
 
     def container_cleanup(self):
-       # TODO: extend this list according to the supported templates
-       templates = ['dom0']
-       for template in templates:
-           for container in self.container.get_container(template):
-               self._container_delete_snapshots(container, template)
+        # clean up temporary snapshots
+        self.agency.clean_container()
+
+        # TODO: extend this list according to the supported templates
+        templates = ['dom0']
+        for template in templates:
+            for container in self.container.get_container(template):
+                self._container_delete_snapshots(container, template)
 
     def _container_delete_snapshots(self, container, template):
         self.retval = self.container.delete_snapshots(container, template)