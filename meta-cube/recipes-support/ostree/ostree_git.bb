OverC/meta-overc

commit 7a464681d15e4611ca37ad7683632f1d53100b0b

ostree: update to 2017.13
To fix compilation issues, we update to 2017.13 and keep in sync
with meta-updater's version.

Signed-off-by: Bruce Ashfield <bruce.ashfield@windriver.com>

meta-cube/recipes-support/ostree/ostree_git.bb
@@ -9,12 +9,12 @@ SRC_URI = " \
     file://0001-autogen.sh-fall-back-to-no-gtkdocize-if-it-is-there-.patch \
 "
 
-SRCREV = "5a5e465492aca13937dab7a2df39f25da94e6e36"
+SRCREV = "ae61321046ad7f4148a5884c8c6c8b2594ff840e"
 
-PV = "2017.8+git${SRCPV}"
+PV = "2017.13+git${SRCPV}"
 S = "${WORKDIR}/git"
 
-inherit autotools-brokensep pkgconfig systemd gobject-introspection
+inherit autotools pkgconfig systemd gobject-introspection
 
 DEPENDS = " \
     glib-2.0 libsoup-2.4 gpgme e2fsprogs \
@@ -45,13 +45,21 @@ EXTRA_OECONF_class-native += " \
 "
 
 # package content
-PACKAGES += "${PN}-systemd-generator"
+PACKAGES += " \
+    ${PN}-systemd-generator \
+    ${PN}-bash-completion \
+"
 
-FILES_${PN} += "${libdir}/girepository-1.0 ${datadir}/gir-1.0"
-FILES_${PN} += "/usr/lib/* /lib/systemd/*"
+FILES_${PN} += " \
+    /usr/lib/girepository-1.0 ${datadir}/gir-1.0 \
+    /usr/lib/tmpfiles.d/ostree*.conf \
+    /usr/lib/* \
+"
 SYSTEMD_SERVICE_${PN} = "ostree-prepare-root.service ostree-remount.service"
 
-FILES_${PN}-systemd-generator = "${libdir}/systemd/system-generators"
+FILES_${PN}-systemd-generator = "/lib/systemd/system-generators"
+FILES_${PN}-bash-completion = "${datadir}/bash-completion/completions/ostree"
+
 
 do_configure_prepend() {
     cd ${S}

commit ae036b104cec1f880380949c2ebd3b6837753565

ostree: fix a parallel compilation issue
A regression was introduced by commit 7a464681:

    ostree: update to 2017.13

    To fix compilation issues, we update to 2017.13 and keep in sync
    with meta-updater's version.

it reverted a following commit 7b436f86:

    ostree: inherit autotools-brokensep instead of autotools

    Autotools class for ostree recipe where separates build dir doesn't
    work well, we have observed quite a few parellel compilation issues, so
    we change it to inherit autotools-brokensep instead, which probably can
    fix those issues, and this is also how autotools is being used in
    ostree recipe of meta-updater project.

this lead ostree fail on parallel compilation again, add it back.

Signed-off-by: Ming Liu <liu.ming50@gmail.com>

meta-cube/recipes-support/ostree/ostree_git.bb
@@ -14,7 +14,7 @@ SRCREV = "ae61321046ad7f4148a5884c8c6c8b2594ff840e"
 PV = "2017.13+git${SRCPV}"
 S = "${WORKDIR}/git"
 
-inherit autotools pkgconfig systemd gobject-introspection
+inherit autotools-brokensep pkgconfig systemd gobject-introspection
 
 DEPENDS = " \
     glib-2.0 libsoup-2.4 gpgme e2fsprogs \